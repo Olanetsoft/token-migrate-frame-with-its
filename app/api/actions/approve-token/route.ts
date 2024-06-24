b9874aef420d7d6ed90b32a7a77f27aabae8319e
Olanetsoft <heedris2olubisi@gmail.com>
2024-06-24 13:21:47 +0100

    fix: convert ethers

--- app/api/actions/approve-token/route.ts
+++ app/api/actions/approve-token/route.ts
@@ -68,7 +68,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
       functionName: "approve",
       args: [
         INTERCHAIN_TOKEN_SERVICE_ADDRESS,
-        parseUnits(amount, 18),
+        parseEther(parseUnits(amount, 18).toString()),
       ],
     });
 
